7239508e24502a585f2f464be925b1f569c99c38
feiilla <401380841@qq.com>
2023-04-22 23:42:00 +0800

    “修改项目代码”

--- src/router/modules/hospitalTextManagement.js
+++
@@ -1,6 +0,0 @@
-export default {
-  path: 'hospitalTextManagement',
-  component: () => import('@/views/hospitalTextManagement/index'),
-  name: 'hospitalTextManagement',
-  meta: { title: '医院信息管理', icon: 'form' }
-}
--- src/views/hospitalGuide/index.vue
+++ src/views/hospitalGuide/index.vue
@@ -6,38 +6,36 @@ import { Viewer } from 'photo-sphere-viewer'
 import 'photo-sphere-viewer/dist/photo-sphere-viewer.css'
 import { MarkersPlugin } from 'photo-sphere-viewer/dist/plugins/markers'
 import 'photo-sphere-viewer/dist/plugins/markers.css'
-import axios from 'axios'
-import { get } from 'js-cookie'
-	  export default {
-		  data() {
-			  return {
-				  viewer: '',
-				  markersPlugin: '',
-				  imgurl1: require('./dating.jpeg'),
-				  imgurl2: require('./shoushu.jpeg'),
-				  imgurl3: require('./zhuyuan.jpeg')
-			  }
-		  },
-		  mounted() {
-			  this.viewer = new Viewer({
-				  container: document.querySelector('#viewer'),
-				  panorama: this.imgurl1,
-				  size: {
-					  width: '95vw',
-					  height: '95vh'
-				  },
-				  plugins: [
-					  [MarkersPlugin, {
-						  	markers: [
-							  {
-                  id: '去手术',
-                  tooltip: '去手术室',
-                  circle: 20,
-                  svgStyle: {
-                  fill: 'rgba(255,255,0,0.3)',
-                  stroke: 'yellow',
-                  strokeWidth: '2px'
-                },
+export default {
+  data() {
+    return {
+      viewer: '',
+      markersPlugin: '',
+      imgurl1: require('./dating.jpeg'),
+      imgurl2: require('./shoushu.jpeg'),
+      imgurl3: require('./zhuyuan.jpeg')
+    }
+  },
+  mounted() {
+    this.viewer = new Viewer({
+      container: document.querySelector('#viewer'),
+      panorama: this.imgurl1,
+      size: {
+        width: '90vw',
+        height: '90vh'
+      },
+      plugins: [
+        [MarkersPlugin, {
+          markers: [
+            {
+              id: '去手术',
+              tooltip: '去手术室',
+              circle: 20,
+              svgStyle: {
+                fill: 'rgba(255,255,0,0.3)',
+                stroke: 'yellow',
+                strokeWidth: '2px'
+              },
               longitude: 1,
               latitude: -0.28
             },
@@ -46,15 +44,27 @@ import { get } from 'js-cookie'
               tooltip: '去住院室',
               circle: 20,
               svgStyle: {
-              fill: 'rgba(255,255,0,0.3)',
-              stroke: 'yellow',
-              strokeWidth: '2px'
-            },
+                fill: 'rgba(255,255,0,0.3)',
+                stroke: 'yellow',
+                strokeWidth: '2px'
+              },
               longitude: 1,
               latitude: 0.28
             },
             {
-              id: 'new-marker',
+              id: '前台角色',
+              tooltip: '前台角色扮演',
+              circle: 20,
+              svgStyle: {
+                fill: 'rgba(255,255,0,0.3)',
+                stroke: 'yellow',
+                strokeWidth: '2px'
+              },
+              longitude: 0,
+              latitude: -0.28
+            },
+            {
+              id: '前台信息',
               circle: 20,
               svgStyle: {
                 fill: 'rgba(255,255,0,0.3)',
@@ -64,17 +74,16 @@ import { get } from 'js-cookie'
               longitude: -0.5,
               latitude: -0.28,
               tooltip: '前台 <b>点击查看详细信息</b>',
-              content: '前台信息读取'
+              content: '前台的主要操作有进行挂号、收取费用、管理病历等工作<p>进入前台角色扮演以查看更多</p>'
             }
-                  // eslint-disable-next-line no-mixed-spaces-and-tabs
-						    ]
-					  }]
-				  ]
-			  })
+          ]
+        }]
+      ]
+    })
     const markersPlugin = this.viewer.getPlugin(MarkersPlugin)
     // any=》手术
     markersPlugin.on('select-marker', (e, marker) => {
-      if (marker.id == '去手术') {
+      if (marker.id === '去手术') {
         this.viewer.animate({
           longitude: marker.config.longitude,
           latitude: marker.config.latitude,
@@ -84,56 +93,166 @@ import { get } from 'js-cookie'
           this.viewer.setPanorama(
             this.imgurl2
           ).then(() =>
-						  	markersPlugin.setMarkers([{
-              id: '去住院',
-              tooltip: '去住院室',
-              circle: 20,
-              svgStyle: {
-                fill: 'rgba(255,255,0,0.3)',
-                stroke: 'yellow',
-                strokeWidth: '2px'
+            markersPlugin.setMarkers([
+              {
+                id: '去住院',
+                tooltip: '去住院室',
+                circle: 20,
+                svgStyle: {
+                  fill: 'rgba(255,255,0,0.3)',
+                  stroke: 'yellow',
+                  strokeWidth: '2px'
+                },
+                longitude: -1.8,
+                latitude: -0.28
               },
-              longitude: -1.8,
-              latitude: -0.28
-            },
-            {
-              id: '去大厅',
-              tooltip: '去大厅',
-              circle: 20,
-              svgStyle: {
-                fill: 'rgba(255,255,0,0.3)',
-                stroke: 'yellow',
-                strokeWidth: '2px'
+              {
+                id: '去大厅',
+                tooltip: '去大厅',
+                circle: 20,
+                svgStyle: {
+                  fill: 'rgba(255,255,0,0.3)',
+                  stroke: 'yellow',
+                  strokeWidth: '2px'
+                },
+                longitude: -1.8,
+                latitude: -0
               },
-              longitude: -1.8,
-              latitude: -0
-            },
-            {
-              id: 'new-marker',
-              circle: 20,
-                    				svgStyle: {
-                fill: 'rgba(255,255,0,0.3)',
-                stroke: 'yellow',
-                strokeWidth: '2px'
+              {
+                id: '医生扮演',
+                tooltip: '医生扮演',
+                circle: 20,
+                svgStyle: {
+                  fill: 'rgba(255,255,0,0.3)',
+                  stroke: 'yellow',
+                  strokeWidth: '2px'
+                },
+                longitude: 0.4,
+                latitude: -0.7
               },
-              longitude: -0.25,
-              latitude: -0.28,
-              tooltip: '手术台 <b>点击查看详细信息</b>',
-              content: '手术台信息读取'
-            }]),
-						  	this.viewer.animate({
+              {
+                id: 'new-marker',
+                circle: 20,
+                svgStyle: {
+                  fill: 'rgba(255,255,0,0.3)',
+                  stroke: 'yellow',
+                  strokeWidth: '2px'
+                },
+                longitude: -0.25,
+                latitude: -0.28,
+                tooltip: '手术台 <b>点击查看详细信息</b>',
+                content: '手术台信息读取'
+              }
+            ]),
+          this.viewer.animate({
             zoom: 50,
             speed: '2rpm'
-						  	}).then(() =>
+          }).then(() =>
             console.log('继续操作')
-						  	)
+          )
           )
         )
       }
     })
-    // any-》住院
+    //  医生扮演
     markersPlugin.on('select-marker', (e, marker) => {
-      if (marker.id == '去住院') {
+      if (marker.id === '医生扮演') {
+        this.viewer.animate({
+          longitude: marker.config.longitude,
+          latitude: marker.config.latitude,
+          zoom: 100,
+          speed: '-2rpm'
+        }).then(() =>
+          this.viewer.setPanorama(
+            this.imgurl2
+          ).then(() =>
+            markersPlugin.setMarkers([
+              {
+                id: '去手术',
+                tooltip: '退出角色扮演',
+                circle: 20,
+                svgStyle: {
+                  fill: 'rgba(255,255,0,0.3)',
+                  stroke: 'yellow',
+                  strokeWidth: '2px'
+                },
+                longitude: -0.25,
+                latitude: -0.28
+              },
+              {
+                id: 'new-marker',
+                circle: 20,
+                svgStyle: {
+                  fill: 'rgba(255,255,0,0.3)',
+                  stroke: 'yellow',
+                  strokeWidth: '2px'
+                },
+                longitude: 0.4,
+                latitude: -0.7,
+                tooltip: '做手术',
+                content: '<h1>手术台操作指南<h1><p></p>'
+              }
+            ]),
+          this.viewer.animate({
+            zoom: 50,
+            speed: '2rpm'
+          }).then(() =>
+            console.log('继续操作')
+          )
+          )
+        )
+      }
+    })
+    //  前台角色扮演
+    markersPlugin.on('select-marker', (e, marker) => {
+      if (marker.id === '前台角色') {
+        this.viewer.animate({
+          longitude: marker.config.longitude,
+          latitude: marker.config.latitude,
+          zoom: 100,
+          speed: '-2rpm'
+        }).then(() =>
+          this.viewer.setPanorama(
+            this.imgurl1
+          ).then(() =>
+            markersPlugin.setMarkers([{
+              id: '去大厅',
+              tooltip: '退出角色扮演',
+              circle: 20,
+              svgStyle: {
+                fill: 'rgba(255,255,0,0.3)',
+                stroke: 'yellow',
+                strokeWidth: '2px'
+              },
+              longitude: -0.5,
+              latitude: -0.28
+            },
+            {
+              id: 'new-marker',
+              circle: 20,
+              svgStyle: {
+                fill: 'rgba(255,255,0,0.3)',
+                stroke: 'yellow',
+                strokeWidth: '2px'
+              },
+              longitude: 0,
+              latitude: -0.28,
+              tooltip: '前台操作',
+              content: '<html><head> <meta content="text/html;charset=UTF-8"></head><body><video width="500" height="500" controls autoplay><source src="/Users/zhanpengfei/petFE/src/views/hospitalGuide/video/qiantai.mp4" type="video/mp4"><source src="/Users/zhanpengfei/petFE/src/views/hospitalGuide/video/qiantai.webm" type="video/webm"><object data="/Users/zhanpengfei/petFE/src/views/hospitalGuide/video/qiantai.mp4" width="320" height="240"><embed width="500" height="500" src="/Users/zhanpengfei/petFE/src/views/hospitalGuide/video/qiantai.swff"></object></video></body></html>'
+            }]),
+          this.viewer.animate({
+            zoom: 50,
+            speed: '2rpm'
+          }).then(() =>
+            console.log('继续操作')
+          )
+          )
+        )
+      }
+    })
+    //  any-》住院
+    markersPlugin.on('select-marker', (e, marker) => {
+      if (marker.id === '去住院') {
         this.viewer.animate({
           longitude: marker.config.longitude,
           latitude: marker.config.latitude,
@@ -143,7 +262,7 @@ import { get } from 'js-cookie'
           this.viewer.setPanorama(
             this.imgurl3
           ).then(() =>
-						  	markersPlugin.setMarkers([{
+            markersPlugin.setMarkers([{
               id: '去大厅',
               tooltip: '去大厅',
               circle: 20,
@@ -154,7 +273,8 @@ import { get } from 'js-cookie'
               },
               longitude: -1,
               latitude: -0.28
-            },{
+            },
+            {
               id: '去手术',
               tooltip: '去手术室',
               circle: 20,
@@ -178,20 +298,80 @@ import { get } from 'js-cookie'
               latitude: -0.28,
               tooltip: '住院箱 <b>点击查看详细信息</b>',
               content: '住院箱信息读取'
-            }]),
-						  	this.viewer.animate({
+            },
+            {
+              id: '医助扮演',
+              tooltip: '扮演医助',
+              circle: 20,
+              svgStyle: {
+                fill: 'rgba(255,255,0,0.3)',
+                stroke: 'yellow',
+                strokeWidth: '2px'
+              },
+              longitude: -0.1,
+              latitude: -0.28
+            }
+            ]),
+          this.viewer.animate({
             zoom: 50,
             speed: '2rpm'
-						  	}).then(() =>
+          }).then(() =>
             console.log('继续操作')
-						  	)
+          )
+          )
+        )
+      }
+    })
+    //  医助扮演
+    markersPlugin.on('select-marker', (e, marker) => {
+      if (marker.id === '医助扮演') {
+        this.viewer.animate({
+          longitude: marker.config.longitude,
+          latitude: marker.config.latitude,
+          zoom: 100,
+          speed: '-2rpm'
+        }).then(() =>
+          this.viewer.setPanorama(
+            this.imgurl3
+          ).then(() =>
+            markersPlugin.setMarkers([{
+              id: '去住院',
+              tooltip: '退出角色扮演',
+              circle: 20,
+              svgStyle: {
+                fill: 'rgba(255,255,0,0.3)',
+                stroke: 'yellow',
+                strokeWidth: '2px'
+              },
+              longitude: -0.5,
+              latitude: -0.28
+            },
+            {
+              id: '去住院',
+              circle: 20,
+              svgStyle: {
+                fill: 'rgba(255,255,0,0.3)',
+                stroke: 'yellow',
+                strokeWidth: '2px'
+              },
+              longitude: 0,
+              latitude: -0.28,
+              tooltip: '医助住院操作',
+              content: '<h1>住院操作指南<h1><blockquote><p>按照医生的诊断进行打针与膳食管理。</p></blockquote>'
+            }]),
+          this.viewer.animate({
+            zoom: 50,
+            speed: '2rpm'
+          }).then(() =>
+            console.log('继续操作')
+          )
           )
         )
       }
     })
     // any->大厅
     markersPlugin.on('select-marker', (e, marker) => {
-      if (marker.id == '去大厅') {
+      if (marker.id === '去大厅') {
         this.viewer.animate({
           longitude: marker.config.longitude,
           latitude: marker.config.latitude,
@@ -201,7 +381,7 @@ import { get } from 'js-cookie'
           this.viewer.setPanorama(
             this.imgurl1
           ).then(() =>
-						  	markersPlugin.setMarkers([{
+            markersPlugin.setMarkers([{
               id: '去手术',
               tooltip: '去手术室',
               circle: 20,
@@ -212,7 +392,20 @@ import { get } from 'js-cookie'
               },
               longitude: 1,
               latitude: -0.28
-            },{
+            },
+            {
+              id: '前台角色',
+              tooltip: '前台角色扮演',
+              circle: 20,
+              svgStyle: {
+                fill: 'rgba(255,255,0,0.3)',
+                stroke: 'yellow',
+                strokeWidth: '2px'
+              },
+              longitude: 0,
+              latitude: -0.28
+            },
+            {
               id: '去住院',
               tooltip: '去住院部',
               circle: 20,
@@ -227,75 +420,27 @@ import { get } from 'js-cookie'
             {
               id: 'new-marker',
               circle: 20,
-                    				svgStyle: {
-                fill: 'rgba(255,255,0,0.3)',
-                stroke: 'yellow',
-                strokeWidth: '2px'
-              },
-              longitude: -0.5,
-              latitude: -0.28,
-              tooltip: '前台 <b>点击查看详细信息</b>',
-              content: '前台信息读取'
-            }
-            ]),
-						  	this.viewer.animate({
-            zoom: 50,
-            speed: '2rpm'
-						  	}).then(() =>
-            console.log('继续操作')
-						  	)
-          )
-        )
-      }
-    })
-    //住院转手术
-    markersPlugin.on('select-marker', (e, marker) => {
-      if (marker.id == '住院') {
-        this.viewer.animate({
-          longitude: marker.config.longitude,
-          latitude: marker.config.latitude,
-          zoom: 100,
-          speed: '-2rpm'
-        }).then(() =>
-          this.viewer.setPanorama(
-            this.imgurl1
-          ).then(() =>
-						  	markersPlugin.setMarkers([{
-              id: '大厅',
-              tooltip: '去手术室',
-              circle: 20,
               svgStyle: {
                 fill: 'rgba(255,255,0,0.3)',
                 stroke: 'yellow',
                 strokeWidth: '2px'
               },
-              longitude: 1,
-              latitude: -0.28
-            },
-            {
-              id: 'new-marker',
-              circle: 20,
-                svgStyle: {
-                fill: 'rgba(255,255,0,0.3)',
-                stroke: 'yellow',
-                strokeWidth: '2px'
-              },
               longitude: -0.5,
               latitude: -0.28,
               tooltip: '前台 <b>点击查看详细信息</b>',
               content: '前台信息读取'
             }
             ]),
-						  	this.viewer.animate({
+          this.viewer.animate({
             zoom: 50,
             speed: '2rpm'
-						  	}).then(() =>
+          }).then(() =>
             console.log('继续操作')
-						  	)
+          )
           )
         )
       }
     })
-		}
-	  }
+  }
+}
 </script>
